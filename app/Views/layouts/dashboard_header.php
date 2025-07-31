8321811f94347516f0284bbf9eb586e0786689be
dvdrobby <david.robby.sanjaya@gmail.com>
2025-07-31 10:23:16 +0700

    tambah logo bbws dan ubah tema ke light

--- app/Views/layouts/dashboard_header.php
+++ app/Views/layouts/dashboard_header.php
@@ -112,12 +112,13 @@
         <!-- /.navbar -->
 
         <!-- Main Sidebar Container -->
-        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+        <aside class="main-sidebar sidebar-light-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="index3.html" class="brand-link">
-                <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">AdminLTE 3</span>
-            </a>
+            <div class="text-center">
+                <img src="dist/img/bbwsbs.png" alt="BBWSBS Logo" class="" style="width:70px">
+                <div class="brand-text font-weight-light">Data Pelanggaran</div>
+            </div>
+
 
             <!-- Sidebar -->
             <div class="sidebar">
