b2e87fdbebd45443e2294ef955ca2faffb7c285c
dvdrobby <david.robby.sanjaya@gmail.com>
2025-08-02 20:53:59 +0700

    ubah tampilan tabel pelanggaran

--- app/Views/pelanggaran/pelanggaran_view.php
+++ app/Views/pelanggaran/pelanggaran_view.php
@@ -20,7 +20,53 @@
           <input type="search" class="form-control form-control-sm" placeholder="" aria-controls="example1">
         </div>
       </div>
-      <div id="jsGrid1"></div>
+      <div class="card-body">
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th style="width: 10px">No</th>
+              <th>Nama Pelanggaran</th>
+              <th>Kota</th>
+              <th>SP</th>
+              <th>PPNS</th>
+              <th style="width: 40px">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1.</td>
+              <td>Update software</td>
+              <td>
+                <div class="progress progress-xs">
+                  <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
+                </div>
+              </td>
+              <td>
+                <div class="progress progress-xs">
+                  <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
+                </div>
+              </td>
+              <td>
+                <div class="progress progress-xs">
+                  <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
+                </div>
+              </td>
+              <td><span class="badge bg-danger">55%</span></td>
+            </tr>
+
+          </tbody>
+        </table>
+      </div>
+      <!-- /.card-body -->
+      <div class="card-footer clearfix">
+        <ul class="pagination pagination-sm m-0 float-right">
+          <li class="page-item"><a class="page-link" href="#">&laquo;</a></li>
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+          <li class="page-item"><a class="page-link" href="#">2</a></li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item"><a class="page-link" href="#">&raquo;</a></li>
+        </ul>
+      </div>
     </div>
     <!-- /.card-body -->
   </div>
